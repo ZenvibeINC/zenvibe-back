242b85a0d9b6c700d1bbc9cd8108a6e887f4f7ab
Juanma <juanma.baal@gmail.com>
2024-09-09 15:41:05 -0500

    The funcionts of the services and controllers carts are created to leave a layout

--- Src/routes/index.ts
+++ Src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { deleteUser, getUsers, registerUser } from "../controllers/usersControllers";
 import { createProduct, deleteProduct, getAllProducts, getProductById } from "../controllers/productsControllers";
+import { addToCart, getCart } from "../controllers/cartsControllers";
 
 const router: Router = Router();
 
@@ -13,5 +14,9 @@ router.get('/products', getAllProducts);
 router.get('/products/:id', getProductById);
 router.post('/products', createProduct);
 router.delete('/products/:id', deleteProduct);
+
+//Rutas Carrito
+router.post('/cart', addToCart);
+router.get('/cart', getCart)
 
 export default router;
