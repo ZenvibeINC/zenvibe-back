b47cdd5895d8bd0a2f5a443e2e4303ebfffb1e6c
Juanma <juanma.baal@gmail.com>
2024-09-09 11:21:00 -0500

    the functions of Product services and controllers are created tobe worked on later when the database is present

---
+++ Src/services/productsServices.ts
@@ -0,0 +1,18 @@
+
+
+export const getAllProductsService = async() => {
+    //logica para traer los productos desde la DB
+}
+
+export const getProductByIdService = async (productId: number) => {
+    //logica para obtener un producto por ID
+}
+
+export const createProductService = async () =>{
+    //input un "productData"
+    //logica para crear el producto
+}
+
+export const deleteProductService = async(id:number): Promise<void> => {
+   //logica para filtrar el producto por id
+}
