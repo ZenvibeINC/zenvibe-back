5b88b69cb8342488da4c83264d52c4295c155842
Juanma <juanma.baal@gmail.com>
2024-09-09 16:06:35 -0500

    The funcionts of the services and controllers orders are created to leave a layout

---
+++ Src/services/orderServices.ts
@@ -0,0 +1,10 @@
+
+export const createOrderService = async( ) => {
+    //input (userId, userData)
+    //logica para crear un pedido
+}
+
+export const getOrderHistoryService = async () => {
+    //input (userId)
+    //logica para obtener historial de pedidos
+}
