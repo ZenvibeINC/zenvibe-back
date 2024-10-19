d4e9e4b42332b7f14909c2d14f9cb0da149f7e4a
Juanma <juanma.baal@gmail.com>
2024-10-18 21:50:29 -0500

    Cors has been implemented to connect back to front

--- Src/server.ts
+++ Src/server.ts
@@ -1,11 +1,16 @@
 import express, { Application } from 'express';
+
 import productsRouter from './routes/productRoutes';
 import router from './routes';
 import cartRouter from './routes/cartRoutes';
 
-
+const cors = require('cors');
 const server: Application = express();
 server.use(express.json());
+server.use(cors({
+    origin: 'http://localhost:3000', // Permite solicitudes desde tu frontend
+    credentials: true, // Si manejas cookies o autenticación
+  }));
 
 //Products
 server.use('/products', productsRouter)
